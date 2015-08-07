4da789cf5e021c8e696a6a26fab5cb980769a53c
zhaogg <19892009111a>
2015-08-07 10:52:50 +0800

    cxzc
    zxc

--- test/src/com/test/Test.java
+++ test/src/com/test/Test.java
@@ -1,9 +1,14 @@
 package com.test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
   public static void main(String[] args1) {
-	System.out.println(556565+"kk1kb");
-	System.out.println(556565+"kk23");
+	  Pattern pattern = Pattern.compile("[A-Za-z]+");
+	  Matcher matcher = pattern.matcher("ASDADFASDsa");
+	  boolean b= matcher.matches();
+	  System.out.println(b);
 
 }
 }
