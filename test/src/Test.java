1d5d4e5a93745639b12d960743d75f6c651d2da2
zhaogg <19892009111a>
2015-08-07 09:20:20 +0800

    6767
    676

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
   public static void main(String[] args1) {
-	System.out.println(556565+"kk1k");
+	System.out.println(556565+"kk1kb");
 	System.out.println(556565+"kk23");
 
 }
